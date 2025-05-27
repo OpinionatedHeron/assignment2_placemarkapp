554bf5c164330c9404865732f540b5138e5f0053
OpinionatedHeron <leamoy@gmail.com>
2025-05-27 08:03:57 +0100

    Adding location form

---
+++ placemark-svelte/src/routes/location/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { subTitle } from "$lib/runes.svelte";
+  import LocationForm from "./LocationForm.svelte";
+
+  subTitle.text = "Add Locations";
+</script>
+
+<LocationForm />
